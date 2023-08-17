5a019eb94a13c797fe9d4151756744b94c681c95
dHarshMakwana <harshmak2211@gmail.com>
2023-08-17 18:27:20 +0530

    feat:home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import s from "./home.module.scss";
+import logo from "/public/logo.png";
+import Image from "next/image";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <div className={s.container}>
+      <div className={s.header}>
+        <Image width={50} alt="" src={logo} />
+        <span>log out</span>
+      </div>
+      <div className={s.hello}>
+        hello <span>user</span>
+      </div>
+      <div className={s.balance}></div>
+      <div className={s.subHeader}>
+        <p>History</p>
+        <span>see all</span>
+      </div>
+    </div>
+  );
 };
 
 export default home;
