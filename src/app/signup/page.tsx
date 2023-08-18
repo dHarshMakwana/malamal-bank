e6cf69185e004156e30099d7cde924c8ef085d86
dHarshMakwana <harshmak2211@gmail.com>
2023-08-18 18:18:50 +0530

    feat:signup function

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import s from "./signup.module.scss";
 import Image from "next/image";
 import logo from "/public/logo.png";
@@ -8,19 +8,58 @@ import Link from "next/link";
 import facebook from "/public/facebook.svg";
 import google from "/public/google.svg";
 import twitter from "/public/twitter.svg";
+import { auth, db } from "@/config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { generateRandomNumber } from "@/utils/randomGenerator";
+import { useRouter } from "next/navigation";
 
 const Signup = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [hasError, setHasError] = useState(false);
-  const iconArray = [google, twitter, facebook];
+  const value = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const iconArray = [
+    { icon: google, onClick: () => console.log("first") },
+    { icon: twitter, onClick: () => console.log("first") },
+    { icon: facebook, onClick: () => console.log("first") },
+  ];
 
-  const handleInputChange = (newValue: string) => {
-    setInputValue(newValue);
-    // You can perform additional validation here and set the error state accordingly
-    setHasError(newValue.includes("error"));
+  const [values, setValues] = useState(value);
+  const randomNumber = generateRandomNumber();
+  const router = useRouter();
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    createUserWithEmailAndPassword(auth, values.email, values.password).then(
+      (user) => {
+        console.log("user ban gaya", user);
+        setDoc(doc(db, "users", user.user.uid), {
+          name: values.name,
+          email: values.email,
+          id: user.user.uid,
+          account: randomNumber,
+          balance: 500,
+          history: [],
+          isVerified: user.user.emailVerified,
+        }).then(() => {
+          console.log("ye bhi ho gaya");
+          router.push("/home");
+        });
+      }
+    );
   };
 
   return (
+    // <AuthContextProvider>
     <div className={s.container}>
       <div className={s.headline}>
         <Image alt="" src={logo} width={100} />
@@ -30,40 +69,45 @@ const Signup = () => {
       </div>
       <div className={s.inputGrp}>
         <Input
-          label="Username"
+          label="name"
           placeholder="john doe"
           onChange={handleInputChange}
-          error={hasError}
+          name="name"
         />
         <Input
           label="Email"
           placeholder="johndoe@gmail.com"
           onChange={handleInputChange}
-          error={hasError}
+          name="email"
         />
         <Input
           label="Password"
           placeholder="johndoe123"
           onChange={handleInputChange}
-          error={hasError}
           type="password"
+          name="password"
         />
       </div>
       <div className={s.connect}>
         <div className="w-full">
-          <div className="btn-primary">Sign Up</div>
+          <div onClick={handleSubmit} className="btn-primary">
+            Sign Up
+          </div>
           <div className={s.link}>
             Already have an account? <Link href="/login">Log in</Link>
           </div>
         </div>
         <span>Or connect with</span>
         <div className={s.iconGrp}>
-          {iconArray.map((icon, i) => (
-            <Image key={i} alt="" src={icon} className={s.icon} />
+          {iconArray.map((item, i) => (
+            <div key={i} onClick={item.onClick} className="">
+              <Image alt="" src={item.icon} className={s.icon} />
+            </div>
           ))}
         </div>
       </div>
     </div>
+    // {/* </AuthContextProvider> */}
   );
 };
 
