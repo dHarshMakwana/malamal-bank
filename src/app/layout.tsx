22f937efd48dffd0c5c72174fed4b610a9e0698e
dHarshMakwana <harshmak2211@gmail.com>
2023-08-25 01:55:04 +0530

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AuthContextProvider>{children}</AuthContextProvider>
+        <>{children}</>
       </body>
     </html>
   );
